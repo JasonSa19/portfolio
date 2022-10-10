3644df2f709900c5c9b11079b0bf9cd5a5fe6683
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-10 21:27:28 +0200

    remove headline component

--- src/components/Sections/Herosection.js
+++ src/components/Sections/Herosection.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Headline from "../Headline";
 
 const Herosection = () => {
-  return <Headline></Headline>;
+  return <h1></h1>;
 };
 
 export default Herosection;
