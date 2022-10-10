5eb1edbc67c8d745b7443186c8c0531a876dee72
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-10 21:27:40 +0200

    delete headline component

--- src/components/Headline.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-const Headline = ({ headlineContent, type }) => {
-  const HeadlineType = `h${this.props.type}`;
-  return <HeadlineType>{headlineContent}</HeadlineType>;
-};
-
-export default Headline;
