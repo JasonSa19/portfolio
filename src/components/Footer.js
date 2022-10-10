8ae0950bb0d854fff97587ad7e5bc71552b2e8eb
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-10 21:07:55 +0200

    init footer menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
 import * as React from "react";
 
 const Footer = () => {
+  const footerMenu = [{ item: 1, navTitle: "Impressum", slag: "imprint" }];
+
   return (
     <footer>
+      <div className="footer-menu"></div>
+      <ul>
+        <li>
+          {footerMenu.map((item) => (
+            <a key={item.slag} href={`#${item.slag}`}>
+              {item.navTitle}
+            </a>
+          ))}
+        </li>
+      </ul>
       <span>Erstellt {new Date().getFullYear()}</span>
     </footer>
   );
