f5f349feca410ce6fb97cb7b2dae006e33a860af
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-11 20:05:13 +0200

    install gatsby image plugin, init start image

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,13 +1,14 @@
 import React from "react";
-import startImg from "/src/images/start.png";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Herosection = () => {
+  const startImg = "../images/start.png";
   return (
     <section className="hero">
       <h1>
         Webdesigner & <strong>Front-End</strong> Entwickler<strong>.</strong>
       </h1>
-      <img src={startImg} alt="Startgrafik" />
+      <StaticImage className="start-image" src={startImg} alt="Start" />
     </section>
   );
 };
