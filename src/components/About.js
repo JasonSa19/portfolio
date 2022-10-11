a99bd18eb5fd4f938d5964a02fa96d888ad40566
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-11 22:25:18 +0200

    style about further, init texts for about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,25 @@ import React from "react";
 const About = () => {
   return (
     <section className="about">
-      <div class="about-textcontent-wrap">
-        <h2>
-          <strong>über&nbsp;</strong>mich<strong>.</strong>
-        </h2>
-        <h3>Hey!</h3>
+      <div className="about-textcontent-wrap">
+        <div className="about-textcontent-headline">
+          <h2>
+            <strong>über&nbsp;</strong>mich<strong>.</strong>
+          </h2>
+          <h3>Hey!</h3>
+          <div className="about-text">
+            <p>
+              Mein Name ist Jason, ich bin 20 Jahre alt und komme aus dem
+              wunderschönen Sauerland in NRW. Auf dieser Seite findest du
+              Informationen und nützliche Fakten, die dir helfen, dich für mich
+              als Ersteller deiner zukünftigen Internetpräsenz zu entscheiden
+              und mich gleichzeitig ein wenig kennenzulernen.{" "}
+              <strong>Let's go!</strong>
+            </p>
+          </div>
+        </div>
       </div>
+      <div className="about-imagecontent-wrap"></div>
     </section>
   );
 };
