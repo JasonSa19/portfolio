5641e7650a381c7ef3dfed0d7c98f11db7d8bdf0
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-11 22:05:17 +0200

    init about section, first styling, init sec-height

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div class="about-textcontent-wrap">
+        <h2>
+          <strong>über&nbsp;</strong>mich<strong>.</strong>
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,15 @@
 // Import required assets and packages
 import * as React from "react";
 import Herosection from "./Herosection";
+import About from "./About";
 
 // Init Components
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   return (
     <main>
       <Herosection />
+      <About />
 
       {children}
     </main>
