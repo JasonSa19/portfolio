a72389a9e2fd0973a9849955be45bcf868f67859
JasonSa19 <jason01sauerwald@gmail.comD>
2022-10-11 21:15:33 +0200

    init svg in start and style

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
-const Herosection = () => {
-  const gitSvg = "/src/images/icon/github.svg";
-  const instagramSvg = "/src/images/icon/github.svg";
-  const mailSvg = "/src/images/icon/github.svg";
-  const linkedinSvg = "/src/images/icon/github.svg";
+import gitSvg from "../images/icon/github.svg";
+import mailSvg from "../images/icon/email-button.svg";
+import instaSvg from "../images/icon/instagram.svg";
+import linkedinSvg from "../images/icon/linkedin-logo.svg";
 
+const Herosection = () => {
   const iconSvg = [
     {
       item: 1,
       slug: "github",
       href: "github.com/jasonsa19",
-      source: { gitSvg },
+      source: gitSvg,
     },
     {
       item: 2,
       slug: "linkedin",
       href: "linkedin.com/in/JasonSauerwald",
-      source: { linkedinSvg },
+      source: linkedinSvg,
     },
     {
       item: 3,
       slug: "mail",
       href: "jason01sauerwald@gmail.com",
-      source: { mailSvg },
+      source: mailSvg,
     },
     {
       item: 4,
       slug: "instagram",
       href: "instagram.com/jason_sa_",
-      source: { instagramSvg },
+      source: instaSvg,
     },
   ];
   const startImg = "../images/start.png";
@@ -40,11 +40,13 @@ const Herosection = () => {
         <h1>
           Webdesigner & <strong>Front-End</strong> Entwickler<strong>.</strong>
         </h1>
-        {iconSvg.map((ic) => (
-          <a key={ic.slug} href={ic.href}>
-            <img src={ic.source}></img>
-          </a>
-        ))}
+        <div className="icon-wrap">
+          {iconSvg.map((ic) => (
+            <a key={ic.slug} href={ic.href}>
+              <img src={ic.source} alt={ic.slug}></img>
+            </a>
+          ))}
+        </div>
       </div>
       <StaticImage className="start-image" src={startImg} alt="Start" />
     </section>
